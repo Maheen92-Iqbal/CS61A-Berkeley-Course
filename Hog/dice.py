aa7fd6dcfbdf321d38ba8193b36c300554fe41d8
Maheen92-Iqbal <maheendars@gmail.com>
2016-10-05 21:03:44 +0500

    Create dice.py

---
+++ Hog/dice.py
@@ -0,0 +1,26 @@
+"""Functions that simulate dice rolls.
+
+A dice function takes no arguments and returns a number from 1 to n
+(inclusive), where n is the number of sides on the dice.
+
+Types of dice:
+
+ -  Dice can be fair, meaning that they produce each possible outcome with equal
+    probability. Examples: four_sided, six_sided
+
+ -  For testing functions that use dice, deterministic test dice always cycle
+    through a fixed sequence of values that are passed as arguments to the
+    make_test_dice function.
+"""
+
+from random import randint
+
+def make_fair_dice(sides):
+    """Return a die that returns 1 to SIDES with equal chance."""
+    assert type(sides) == int and sides >= 1, 'Illegal value for sides'
+    def dice():
+        return randint(1,sides)
+    return dice
+
+four_sided = make_fair_dice(4)
+six_sided = make_fair_dice(6)
